f2ceb1297d5eb692f93fcc656d5d930522ba3a11
Nihad <nihad.nemetli@gmail.com>
2026-06-06 21:22:51 +0400

    window created

--- youtube/hello-world-to-sdl/main.c
+++ youtube/hello-world-to-sdl/main.c
@@ -2,5 +2,16 @@
 
 int main() {
 
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Failed!");
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Hello World", 800, 600, 
+        SDL_WINDOW_RESIZABLE);
+
+    SDL_Delay(50000);
+    SDL_Quit();
+
     return 0;
 }
