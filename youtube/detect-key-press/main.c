a79dbc62ce41a7808f2dcbbbc31d62da5f821929
Nihad <nihad.nemetli@gmail.com>
2026-06-07 16:14:13 +0400

    add: rendering part added to see window

--- youtube/detect-key-press/main.c
+++ youtube/detect-key-press/main.c
@@ -1,25 +1,39 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
 
-
-
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Detect Key Press", 800, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow(
+        "Detect Key Press",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    );
 
-    SDL_Event event;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
     bool running = true;
+    SDL_Event event;
 
     while (running) {
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
-                break; 
+                running = false;
+            } 
+            else if (event.type == SDL_EVENT_KEY_DOWN) {
+                SDL_Log("Key Pressed");
             }
         }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
